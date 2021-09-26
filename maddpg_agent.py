024b2c5883749c037e5a02fc238e3c1c0b0c4bec
Gurchetan <gurchetan.singh@kvantum.ai>
2021-09-27 00:59:21 +0530

    Final commit

--- maddpg_agent.py
+++ maddpg_agent.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 512        # minibatch size
+BATCH_SIZE = 256        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 6e-2              # for soft update of target parameters
 
